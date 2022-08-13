c8b036ed6bf7394b4ce1362043a9ec94298a195e
manueleaf <59857920+manueleaf@users.noreply.github.com>
2022-08-13 14:55:07 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -2,26 +2,38 @@
     include_once 'header.php';
 ?>
 
-<div class="wrapper">
+<link rel="stylesheet" href="mystyle.css">
 
-<section class="index-intro">
-   <h2>Log in</h2>
-   <form action="includes/logininc.php" method="post">
-    <input type="text" name="name" placeholder="Username/Correo">
-    <input type="password" name="pwd" placeholder="Contraseña">
-    <button type="submit" name="submit">Ingresar</button>
-   </form>
-</section>
-
-<?php
+<div class="form2">
+      <div class="title">Iniciar sesion</div>
+      <form action="includes/logininc.php" method="post">
+      <div class="input-container ic1">
+        <input id="name" class="input" type="text" name="name" placeholder=" " />
+        <div class="cut cut-uid"></div>
+        <label for="name" class="placeholder">Usuario/Correo</label>
+      </div>
+      <div class="input-container ic2">
+        <input id="pwd" class="input" type="password" name="pwd" placeholder=" " />
+        <div class="cut"></div>
+        <label for="pwd" class="placeholder">Contraseña</>
+      </div>
+      <?php
 if (isset($_GET["error"])){
     if($_GET["error"]=="emptyinput"){
-       echo "<p>¡Llena todos los campos!</p>";
+      echo "<div class='subtitle'>¡Llena todos los campos!</div>";
     } else if($_GET["error"]=="wronglogin"){
-        echo "<p>Usuario o contraseña incorrectos</p>";
+      echo "<div class='subtitle'>Usuario o contraseña incorrecto</div>";
      }
-}
+}else
+ echo "<div class='empty'>.</div>";
+
 ?>
+      <button type="submit" name="submit" class="submit">Ingresar</button>
+      </form>
+    </div>
+
+
+
 
 <?php
     include_once 'pie.php';
